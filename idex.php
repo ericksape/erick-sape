8010c60d1d79a04242643d41f5681766153b6483
erick <law_erick@hotmail.com>
2016-02-29 17:53:28 -0500

    estructura de proyecto

---
+++ idex.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>erick flores</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee behavior="" direction="" >
+erick flores
+</marquee>
+<img src="lupe.jpg" alt="">
+<img src="lupe.jpg" alt="">
+<marquee behavior="" direction="" >
+erick flores
+</marquee>
+</body>
+</html>
